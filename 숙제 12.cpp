54157842a3b23921e5b5c73d6b6500a4758c31e9
juwon201 <wlswndnjs201@naver.com>
2025-08-20 20:37:29 +0900

    Add files via upload

---
+++ 숙제 12.cpp
@@ -0,0 +1,120 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#define MAXSIZE 10
+
+class CQueue {
+public:
+	int queue[MAXSIZE];
+	int front, rear;
+
+	void init_queue(void)
+	{
+		front = rear = 0;
+		printf("QUEUE SIZE is %d\n\n", MAXSIZE);
+	}
+	void clear_queue(void)
+	{
+		front = rear;
+	}
+	int enqueue(int val)
+	{
+		if ((rear + 1) % MAXSIZE == front)
+		{
+			printf("Queue Overflow, \n");
+			return -1;
+		}
+		queue[rear] = val;
+		rear = ++rear % MAXSIZE;
+		return val;
+	}
+	int dequeue(void)
+	{
+		int i;
+		if (front == rear)
+		{
+			printf("Queue Underflow, \n");
+			return -1;
+		}
+		i = queue[front];
+		front = ++front % MAXSIZE;
+
+		return i;
+	}
+	void print_queue(void)
+	{
+		int i;
+		printf("Queue From Front ----> To Rear \n");
+		for (i = front; i != rear; i = ++i % MAXSIZE)
+			printf("%-6d", queue[i]);
+		printf("\n\n");
+	}
+	void save(void)
+	{
+		FILE* wfp = fopen("queue.txt", "w");
+
+		fprintf(wfp, "%d %d\n", front, rear);
+
+		for (int i = front; i != rear; i = ++i % MAXSIZE)
+			fprintf(wfp, "%d\n", queue[i]);
+
+		fclose(wfp);
+	}
+	void load(void)
+	{
+		FILE* rfp = fopen("queue.txt", "r");
+		if (rfp != NULL)
+		{
+			fscanf(rfp, "%d %d", &front, &rear);
+
+			for (int i = front; i != rear; i = ++i % MAXSIZE)
+				fscanf(rfp, "%d", &queue[i]);
+			fclose(rfp);
+		}
+	}
+};
+
+
+
+void main()
+{
+	CQueue qu;
+	
+	bool while_flag = true;
+	qu.init_queue();
+	qu.load();
+
+	while (while_flag)
+	{
+		int choice;
+
+		printf("[Queue]\n1. enqueue\n2. dequeue\n3. clear\n: ");
+		scanf("%d", &choice);
+
+		switch (choice)
+		{
+		case 1:
+			int value;
+
+			printf("Queue에 넣을 값을 입력해주세요 : ");
+			scanf("%d", &value);
+
+			qu.enqueue(value);
+			break;
+		case 2:
+			int i;
+
+			i = qu.dequeue();
+			if (i != -1)
+				printf("빠져 나온 값 : %d\n", i);
+			break;
+		case 3:
+			qu.clear_queue();
+			break;
+		default:
+			while_flag = false;
+			break;
+		}
+		qu.print_queue();
+	}
+	qu.save();
+}
